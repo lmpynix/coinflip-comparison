208c09b56099ca3b313d1e5415fc96a6742672e3
Logan Power <lmpower2@illinois.edu>
2018-09-11 11:45:06 -0700

    Finished data analyzer, so now data can actually be analyzed. Run the data analyzer with "python3 ./dataanalyzer.py" right now since it isn't executable.

--- dataanalyzer.py
+++ dataanalyzer.py
@@ -1,4 +1,29 @@
 import json
 
 def data_analyzer(data_list: list) -> None:
-	
+        """
+        Takes in a list of lists of lists spit out by the score generator.
+        It then uses a best-so-far system to determine the best streak for each streak length.
+        """
+        high_scores = [] # List of lists.  Form [[streaklength, score], [...], ...]
+        for streaklength_list in data_list:
+            highest_score = [] # Best so far variable.
+            for streak in streaklength_list: # Each specific streak.  Form [samplelength, startpt, wins, score].
+                if (highest_score == [] or streak[3] > highest_score[3]):
+                    highest_score = streak
+            high_scores.append(highest_score)
+        # Data accumulated and sorted.  Now we need to export it or print it out in a readable format.
+        print("Streak Length | Highest Score | Start Position | Wins")
+        for highscore_entry in high_scores:
+            print("%13d | %13f | %14d | %13d" % (highscore_entry[0], highscore_entry[3], highscore_entry[1], highscore_entry[2]))
+        return high_scores
+
+
+if __name__ == "__main__":
+    raw_data = []
+    with open(input("Raw data file name > "), "r") as infile:
+        raw_data = json.load(infile)
+    hiscore_data = data_analyzer(raw_data)
+    with open(input("Output file name > "), "w") as outfile:
+        json.dump(hiscore_data, outfile, indent=4)
+        print("Results stored!")
