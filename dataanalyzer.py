a4337086fbd36ac71bfa19d030b07a2a4ddaa9c2
Logan Power <lmpower2@illinois.edu>
2018-09-11 11:12:59 -0700

    Added the code I had before I started the repo.

---
+++ dataanalyzer.py
@@ -0,0 +1,4 @@
+import json
+
+def data_analyzer(data_list: list) -> None:
+	
---
+++ streakscore/streakscore.py
@@ -0,0 +1,39 @@
+# Streak analyzer: Takes an input of a binary patterned string (e.g. WLLLWLLWLWLWLWLWLW) and interprets the
+# length of streaks and the probability those streaks would occur randomly.
+import json
+from scipy import special
+
+STARTLENGTH = 3  # The length of sequence we start looking at.  There's no point to say they won 2 out of the last 2.
+POSITIVEVALUE = "1" # Character we are looking for as being a positive thing.
+
+
+def streak_analyzer(data: str) -> list:
+    resultlist = list()
+    winaccum = int()
+    datalength = len(data)
+    NMAX = datalength
+    for samplelength in range(STARTLENGTH, NMAX):  # For every sample length between the minimum and maximum,
+        print("New sample length: " + str(samplelength))
+        lengthresultlist = []
+        for startpt in range(0, datalength - samplelength):  # For every startpoint the length could have,
+            print("Running start point " + str(startpt) + " of " + str(datalength - samplelength))
+            winaccum = 0  # Re-initialize the win accumulator and the score for this particular set of data points.
+            score = 0
+            for entryind in range(startpt,
+                                  startpt + samplelength):  # For every entry inside the sample at this start pt,
+                if data[entryind] == POSITIVEVALUE:  # If this particular entry is a good one, increment the win accum.
+                    winaccum += 1
+            score = 1 / coin_flip_prob(winaccum, samplelength)
+            lengthresultlist.append([samplelength, startpt, winaccum, score])
+        resultlist.append(lengthresultlist)
+    return resultlist
+
+def coin_flip_prob(wins: int, n: int) -> float:
+    return (special.binom(n, wins) * 0.5**wins * (0.5)**(n - wins))
+
+if __name__ == "__main__":
+    data = streak_analyzer(input("Data string > "))
+    with open("streakout.json", "w") as outfile:
+        json.dump(data, outfile, indent=4)
+    print("Results stored in JSON format in streakout.txt")
+
